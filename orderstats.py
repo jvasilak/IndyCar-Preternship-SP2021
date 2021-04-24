1fb174a0b4b69f14d05de733572a4dfc12ce5777
Mark Schermerhorn <mscherm2@nd.edu>
2021-04-24 16:50:57 -0400

    Order JSON File by Time

--- orderstats.py
+++ orderstats.py
@@ -3,39 +3,38 @@
 import sys
 import json
 
-race = []
+race = {}
 newList = []
+overtakePassingTime = []
+racePassingID = []
 currentTime = 0
-for line in sys.stdin:
-    line = json.loads(line)
-    race.append(line)
 
-for i in range(0,len(race)):
-    if 'ElapsedTime' in race[i]:
-        if race[i]['ElapsedTime'] == time:
-            newList.append(race[i])
-            del race[i]
-                
-    elif 'OvertakeNo' in race[i]:
-        for entry2 in race:
-            try:
-                if race[i]['PassingTime'] == entry2['PassingTime'] and entry2['ElapsedTime'] == time:
-                    newList.append(race[i])
-                    del race[i]
-                    break
-            except:
-                continue
-                        
-    elif 'ForPosition' in race[i]:
-        for entry2 in race:
-            try:
-                if entry['PassingID'] == entry2['PassingID'] and entry2['ElapsedTime'] == time:
-                    newList.append(entry)
-                    del race[i]
-                    break
-            except:
-                continue
-                        
+with open('2019RoadAmerica.json') as json_file:
+            race = json.load(json_file)
+
+for newEntry in race['Overtakes']:
+    overtakePassingTime.append(newEntry['PassingTime'])
+
+for newEntry in race['RacePasses']:
+    racePassingID.append(newEntry['PassingID'])
+    
+pastEntry = {'PassingTime':0}
+for newEntry in race['Passings']:
+    newList.append(newEntry)
+    if newEntry['PassingID'] in racePassingID:
+        for i in range(0, len(racePassingID)):
+            if newEntry['PassingID'] == racePassingID[i]:
+                newList.append(race['RacePasses'][i])
+                break
+    for i in range(0,len(overtakePassingTime)):
+        if overtakePassingTime[i] < pastEntry['PassingTime']:
+            continue
+        elif overtakePassingTime[i] <= newEntry['PassingTime']:
+            newList.append(race['Overtakes'][i])
+        elif overtakePassingTime[i] > newEntry['PassingTime']:
+            break
+
+    pastEntry['PassingTime'] = newEntry['PassingTime']
 
 for line in newList:
     print(line)
