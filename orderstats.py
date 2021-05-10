5fdff9e2cf0791a6f9d9efb7eabcf05c6ade3880
Mark Schermerhorn <mscherm2@nd.edu>
2021-05-10 00:41:03 -0400

    orderstats.py changes

--- orderstats.py
+++ orderstats.py
@@ -1,18 +1,37 @@
 #!/usr/bin/env python3
+#
+# IndyCar Preternship Team - CSE 20312
+# Preternship Software Engineers: Mark Schermerhorn, Jonathon Vasilak, Emma Fredin, and Aidan Gordon
+# May 12, 2021
+#
+# This program is meant to be used with past test cases to put JSON files into the correct chronological order and format to be read by IndyCarPreternshipFINAL.py
+# The test case should be entered as the first command-line argument.
 
 import sys
 import json
 import re
 
+# Initialize data structures and variables
 race = {}
 newList = []
 overtakePassingTime = []
 racePassingID = []
 currentTime = 0
 
-with open('2019RoadAmerica.json') as json_file:
+# Exit if test case not given
+if len(sys.argv) < 2:
+    print("No test-case given. Program shut-down")
+    exit(1)
+
+if len(sys.argv) > 2:
+    print("Too many arguments given. Program shut-down")
+    exit(1)
+
+# open JSON file
+with open(sys.argv[1]) as json_file:
     race = json.load(json_file)
 
+# Order entries by Elapsed Time
 for newEntry in race['Overtakes']:
     overtakePassingTime.append(newEntry['PassingTime'])
 
@@ -38,6 +57,7 @@ for newEntry in race['Passings']:
 
     pastEntry['PassingTime'] = newEntry['PassingTime']
 
+# Execute string substitutions to convert lines to acceptable Python code for IndyCarPreternshipFINAL.py
 for line in newList:
     entry = str(line)
     if "False" in entry:
